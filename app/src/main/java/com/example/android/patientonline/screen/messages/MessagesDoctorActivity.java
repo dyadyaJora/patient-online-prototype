ec0deb4d235a3d4bc090b3535dcbedfa5bb086b0
dyadyaJora <ja_massa97@mail.ru>
2018-05-05 02:53:11 +0300

    stub для странички с письмами\уведомлениями

---
+++ app/src/main/java/com/example/android/patientonline/screen/messages/MessagesDoctorActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.patientonline.screen.messages;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.patientonline.R;
+
+public class MessagesDoctorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messages_doctor);
+    }
+}
